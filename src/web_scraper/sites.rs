863f37d811b6109d091275957b68a18f401c5167
Hazem <hazemfarah26@gmail.com>
2026-05-05 07:27:07 +0100

    SpaceNet

--- src/web_scraper/sites.rs
+++ src/web_scraper/sites.rs
@@ -12,6 +12,7 @@ use tokio::time::sleep;
 use crate::web_scraper::sites::gamershop::GamerShop;
 use crate::web_scraper::sites::megapc::MegaPC;
 use crate::web_scraper::sites::mytek::Mytek;
+use crate::web_scraper::sites::spacenet::SpaceNet;
 use crate::web_scraper::sites::tunisianet::Tunisianet;
 use crate::web_scraper::sites::utils::ElementRefExt;
 
@@ -21,6 +22,7 @@ pub mod skymil_shop;
 pub mod mytek;
 pub mod gamershop;
 pub mod megapc;
+pub mod spacenet;
 
 pub static PAGE_CACHE: Lazy<RwLock<HashMap<String, HashMap<String, Product>>>> = Lazy::new(|| RwLock::new(HashMap::new()));
 const MAX_RETRIES: i32 = 3;
@@ -32,7 +34,29 @@ pub static SITES: Lazy<Vec<Box<dyn Site>>> = Lazy::new(|| vec![
     // Box::new(SkyMilShop),
     // Box::new(Mytek),
     // Box::new(GamerShop),
-    Box::new(MegaPC),
+    // Box::new(MegaPC),
+    Box::new(SpaceNet),
+
+
+    //zstore.com.tn
+    //wiki.tn
+    //mbm-tn.com
+    //www.planete-informatique.tn
+    //www.sbsinformatique.com
+    //www.expert-gaming.tn
+    //bestbuytunisie.tn
+    //clickup.tn
+    //www.technopro-online.com
+    //tdiscount.tn
+    //https://www.scoopgaming.com.tn/
+    //https://www.scoop.com.tn/
+    //qsnet.tn
+    //www.mediavision.tn
+    //tunewtec.com
+    //kamounhome.tn
+    //infotec.tn
+    //sig-shop.tn
+    //carthagoinformatique.tn
 ]);
 
 pub struct SiteConfig {
